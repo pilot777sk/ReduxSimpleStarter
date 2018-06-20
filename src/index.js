293e0cacc1c25cbc020afa0162a60ecf1e7f738c
Steve <>
2018-06-20 09:31:39 -0400

    .

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+
+
 const API_KEY = 'AIzaSyCqgE0zVyA8wiIY756fBJxquXcKd7PKyX8';
 
 
@@ -16,8 +18,12 @@ class App extends Component {
        videos: [],
        selectedVideo: null 
     };
+    this.videoSearch('surfboards');
+    
+  }
 
-    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+  videoSearch(term){
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({ 
         videos: videos,
         selectedVideo: videos[0]
@@ -28,9 +34,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
